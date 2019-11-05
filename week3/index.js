6f629af9d8188be387126400f5b57e47bb2bf6a5
Matteo Guzzo <guzzo.matteo@gmail.com>
2019-11-05 11:49:43 +0100

    Add model-service pattern and use it in index.js

--- week3/index.js
+++ week3/index.js
@@ -1,107 +1,129 @@
 // Non-smoking bars maps - An app to find non-smoking bars or bars with non-smoking areas
-(async () => {
-    try {
-
 const Bar = require("./models/bar")
+const BarService = require("./services/bar-service")
 const District = require("./models/district")
+const DistrictService = require("./services/district-service")
 const db = require("./models/database")
 var NodeGeocoder = require('node-geocoder')
 
-// Utility functions
-log = console.log
-arrayToListedString = ratingsArray => ratingsArray
-        .reduce((prev, next) => prev + next + "\n", "")
+async function main() {
+    try {
 
-// ----------------------
-// User code
-// ----------------------
+        // Utility functions
+        log = console.log
+        arrayToListedString = ratingsArray => ratingsArray
+            .reduce((prev, next) => prev + next + "\n", "")
 
-// Create some bars
-const badBar = new Bar("Ecke-Kneipe", "Schönhauser Allee 13, Berlin", true, false)
-const familyBar = new Bar("Weinberg", "Oranienstrasse 13, Berlin", false, false)
-// Create a district
-const pBerg = new District("Prenzlauer Berg")
-// Add bars to the district
-pBerg.addBar(badBar)
-pBerg.addBar(familyBar)
+        // ----------------------
+        // User code
+        // ----------------------
 
-// Add a bad review to the bad bar
-badBar.addReview("You can't breath in there", 0.1)
-// Add a good review to the good bar
-familyBar.addReview("Great place!", 5)
-// and so on...
-badBar.addReview("Great place, great atmosphere, cheap.", 5)
-badBar.addReview("Pretty smoky but also dirt cheap.", 3)
-familyBar.addReview("It's ok.", 4)
-familyBar.addReview("Very nice bar.", 4)
-familyBar.addReview("Quiet, nice, great service.", 5)
-// Promote a bar in the ditrict
-pBerg.promoteBar(familyBar)
+        // Create some bars
+        const badBar = new Bar("Ecke-Kneipe", "Schönhauser Allee 13, Berlin", true, false)
+        const familyBar = new Bar("Weinberg", "Oranienstrasse 13, Berlin", false, false)
+        // Create a district
+        const pBerg = new District("Prenzlauer Berg")
+        // Add bars to the district
+        pBerg.addBar(badBar)
+        pBerg.addBar(familyBar)
 
-log("Sample district object: ")
-log(pBerg)
-log('-----------------------------------------------------------------')
-log("Sample bar object: ")
-log(familyBar)
-log('-----------------------------------------------------------------')
-log("Sample review object: ")
-log(familyBar.reviews[0])
-log('-----------------------------------------------------------------')
-log("Bars and ratings in " + pBerg.name + ": ")
-const barsRatings = pBerg.bars.map(bar => bar.name 
-    + ": " + bar.getRating() 
-    + " (" + bar.reviews.length + " reviews)")
-log(arrayToListedString(barsRatings))
+        // Add a bad review to the bad bar
+        badBar.addReview("You can't breath in there", 0.1)
+        // Add a good review to the good bar
+        familyBar.addReview("Great place!", 5)
+        // and so on...
+        badBar.addReview("Great place, great atmosphere, cheap.", 5)
+        badBar.addReview("Pretty smoky but also dirt cheap.", 3)
+        familyBar.addReview("It's ok.", 4)
+        familyBar.addReview("Very nice bar.", 4)
+        familyBar.addReview("Quiet, nice, great service.", 5)
+        // Promote a bar in the ditrict
+        pBerg.promoteBar(familyBar)
 
-log('-----------------------------------------------------------------')
-const barWithReviews = pBerg.bars[0]
-log('Reviews for ' + barWithReviews.name + ":")
-const barReviews = barWithReviews.reviews.map(review => 
-    "Rating: " + review.rating + "\n"
-    + "Review: " + review.text
-)
+        log("Sample district object: ")
+        log(pBerg)
+        log('-----------------------------------------------------------------')
+        log("Sample bar object: ")
+        log(familyBar)
+        log('-----------------------------------------------------------------')
+        log("Sample review object: ")
+        log(familyBar.reviews[0])
+        log('-----------------------------------------------------------------')
+        log("Bars and ratings in " + pBerg.name + ": ")
+        const barsRatings = pBerg.bars.map(bar => bar.name
+            + ": " + bar.getRating()
+            + " (" + bar.reviews.length + " reviews)")
+        log(arrayToListedString(barsRatings))
 
-log(arrayToListedString(barReviews))
-log('-----------------------------------------------------------------')
-log('Like and dislike reviews, sort them by number of likes:')
-pBerg.bars[0].reviews[0].dislike()
-pBerg.bars[0].reviews[1].like()
-log(pBerg.bars[0].reviews.sort((a, b) => b.likes - a.likes))
+        log('-----------------------------------------------------------------')
+        const barWithReviews = pBerg.bars[0]
+        log('Reviews for ' + barWithReviews.name + ":")
+        const barReviews = barWithReviews.reviews.map(review =>
+            "Rating: " + review.rating + "\n"
+            + "Review: " + review.text
+        )
 
-log('-----------------------------------------------------------------')
-log('Store data and load it back:')
-const fileName = "pberg.json"
-db.save(fileName, pBerg)
+        log(arrayToListedString(barReviews))
+        log('-----------------------------------------------------------------')
+        log('Like and dislike reviews, sort them by number of likes:')
+        pBerg.bars[0].reviews[0].dislike()
+        pBerg.bars[0].reviews[1].like()
+        log(pBerg.bars[0].reviews.sort((a, b) => b.likes - a.likes))
 
-log("Loaded data from disk:")
+        log('-----------------------------------------------------------------')
+        log('Store data and load it back:')
+        const fileName = "pberg.json"
+        db.save(fileName, pBerg)
+
+        log("Loaded data from disk:")
 
         loadedFile = await db.load(fileName)
 
-const loadedDistrict = District.create(loadedFile)
+        const loadedDistrict = District.create(loadedFile)
 
-console.log(loadedDistrict)
+        console.log(loadedDistrict)
 
-log('---------------------------------------------------------------')
-// log('Geocoding address `' + familyBar.address + '`...')
-// // Find exact address with geocoder library
-// var options = {
-//   provider: 'openstreetmap',
-//   // Optional depending on the providers
-//   httpAdapter: 'https', // Default
-//   // apiKey: 'YOUR_API_KEY', // for Mapquest, OpenCage, Google Premier
-//   formatter: null         // 'gpx', 'string', ...
-// };
- 
-// var geocoder = NodeGeocoder(options)
-// // Using callback
-// geocoder.geocode(familyBar.address)
-//   .then(function(res) {
-//   console.log(res);
-//   })
-//   .catch(function(err) {
-//     console.log(err);
-//   });
+        log('---------------------------------------------------------------')
+        // Add bars using the service
+        await BarService.add(badBar)
+        await BarService.add(familyBar)
+        log("BarService.findAll:")
+        const bars = await BarService.findAll()
+        log(bars)
+        log("-------------------------------------")
+        // Add District with service
+        await DistrictService.add(pBerg)
+        log("DistrictService.findAll:")
+        const mydist = await DistrictService.findAll()
+        log(mydist)
+        log("-------------------------------------")
+        log("BarService.find():")
+        foundBar = await BarService.find(0)
+        log(foundBar)
+        log("-------------------------------------")
+        // Find exact address with geocoder library
+        log('Geocoding address `' + familyBar.address + '`...')
+        var options = {
+            provider: 'openstreetmap',
+            // Optional depending on the providers
+            httpAdapter: 'https', // Default
+            // apiKey: 'YOUR_API_KEY', // for Mapquest, OpenCage, Google Premier
+            formatter: null         // 'gpx', 'string', ...
+        };
+
+        var geocoder = NodeGeocoder(options)
+        // Using callback
+        await geocoder.geocode(familyBar.address)
+            .then(function (res) {
+                console.log(res);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+
     } catch (e) {
         log(e)
     }
-})()
+}
+
+main()
