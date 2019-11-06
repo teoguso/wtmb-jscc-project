794a99122e3d2002ca56d974b3bc9c42a72ce2c0
Matteo Guzzo <guzzo.matteo@gmail.com>
2019-11-06 19:28:31 +0100

    Add and fix district api and view

--- week4/index.js
+++ week4/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const BarService = require('./services/bar-service')
+const DistrictService = require('./services/district-service')
 
 const app = express()
 
@@ -15,7 +16,6 @@ app.get('/', (request, response) => {
 
 app.get('/bar/all', async (req, res) => {
   const bars = await BarService.findAll()
-//   res.send(bars)
   res.render('bar', { bars: bars })
 })
 
@@ -28,13 +28,35 @@ app.get('/bar/:id', async (req, res) => {
 app.post('/bar', async (req, res) => {
   const body = req.body
   bar = await BarService.add(body)
-  // console.log(person)
   res.send(bar)
 })
 
 app.delete('/bar/:id', async (req, res) => {
   id = req.params.id
   await BarService.del(id)
+  res.send('OK')
+})
+
+app.get('/district/all', async (req, res) => {
+  const districts = await DistrictService.findAll()
+  res.render('district', { districts: districts })
+})
+
+app.get('/district/:id', async (req, res) => {
+  const id = req.params.id
+  const district = await DistrictService.find(id)
+  res.send(district)
+})
+
+app.post('/district', async (req, res) => {
+  const body = req.body
+  district = await DistrictService.add(body)
+  res.send(district)
+})
+
+app.delete('/district/:id', async (req, res) => {
+  id = req.params.id
+  await DistrictService.del(id)
   res.send('OK')
 })
 
